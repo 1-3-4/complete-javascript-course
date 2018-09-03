d3274f8c819213dcdc35dd9f5edd728aa6de9f91
134 <38720563+1-3-4@users.noreply.github.com>
2018-09-03 12:01:31 +0200

    Started on solving coding challenge 6.2
    It is not working correctly yet.

--- 4-DOM-pig-game/lecture-54-coding-challenge-6/app.js
+++ 4-DOM-pig-game/lecture-54-coding-challenge-6/app.js
@@ -14,7 +14,7 @@ GAME RULES:
 VARIABLE(S)
 ---------*/
 
-var globalScores, roundScore, activePlayer, dice, previousDice;
+var globalScores, roundScore, activePlayer, dice, previousDice, winningScore;
 
 // State variable: A variable that tells us the condition of a system.
 var gameIsPlaying;
@@ -75,7 +75,8 @@ document.querySelector( '.btn-hold' ).addEventListener( 'click', function() {
         document.getElementById( 'score-' + activePlayer ).textContent = globalScores[activePlayer];
         document.getElementById( 'current-' + activePlayer ).textContent = roundScore;
         // 3. Check if the player has won the game:
-        if (globalScores[activePlayer] >= 100 ) {
+        // if (globalScores[activePlayer] >= 100 ) {
+        if (globalScores[activePlayer] >= winningScore ) {
             // alert('Player ' + ( activePlayer + 1 ) + ' has won the pig game with ' + globalScores[activePlayer] + ' points!');
             document.querySelector( '#name-' + activePlayer ).textContent = 'WINNER!';
             document.querySelector('.dice').style.display = 'none';
@@ -115,28 +116,49 @@ function nextPlayer() {
     previousDice = 0;
 }
 function init() {
-    // Set state variable to true:
-    gameIsPlaying = true;
-    // Reset global scores:
-    globalScores = [0, 0];
-    // Reset current score:
-    roundScore = 0;
-    // Set active player to 0:
-    activePlayer = 0;
-    // Set previous dice roll to 0:
-    previousDice = 0;
-    // Hide die on table:
-    document.querySelector( '.dice' ).style.display = 'none';
-    // Update UI:
-    document.getElementById( 'name-0').textContent = 'Player 1';
-    document.getElementById( 'name-1').textContent = 'Player 2';
-    document.getElementById('score-0').textContent = 0;
-    document.getElementById('score-1').textContent = 0;
-    document.getElementById('current-0').textContent = 0;
-    document.getElementById('current-1').textContent = 0;
-    document.querySelector('.player-0-panel').classList.remove('winner');
-    document.querySelector('.player-1-panel').classList.remove('winner');
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add('active');
+    // Read chosen winning score from input field and save to variable:
+    var winningScoreInput = document.getElementById('winning-score').value;
+        console.log('Winning score input: ' + winningScoreInput);
+
+    // Change winning score data type to a number if possible:
+    winningScore = parseInt( winningScoreInput );
+        console.log(winningScore);
+
+    // Check if a winning score has been typed:
+    if (winningScoreInput === '') {
+        alert('Please set a winning score.');
+    }
+
+    // Else check if wished for winning score is a number:
+    else if ( winningScore == NaN ) {
+        alert('Winning score is not a number! Try again.');
+    }
+
+    // Else initialize game:
+    else {
+        // Set state variable to true:
+        gameIsPlaying = true;
+        // Reset global scores:
+        globalScores = [0, 0];
+        // Reset current score:
+        roundScore = 0;
+        // Set active player to 0:
+        activePlayer = 0;
+        // Set previous dice roll to 0:
+        previousDice = 0;
+        // Hide die on table:
+        document.querySelector( '.dice' ).style.display = 'none';
+        // Update UI:
+        document.getElementById( 'name-0').textContent = 'Player 1';
+        document.getElementById( 'name-1').textContent = 'Player 2';
+        document.getElementById('score-0').textContent = 0;
+        document.getElementById('score-1').textContent = 0;
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.remove('active');
+        document.querySelector('.player-0-panel').classList.add('active');
+    }
 }
