e0dabcc1a93a7887ff84ca872caff4d138faf442
134 <38720563+1-3-4@users.noreply.github.com>
2018-10-24 12:01:49 +0200

    Started lesson 78.

---
+++ 6-budgety/lecture-78-setting-up-the-first-event-listeners/app.js
@@ -0,0 +1,27 @@
+/*------------------------------------------------------------------------------
+BUDGET CONTROLLER
+------------------------------------------------------------------------------*/
+var budgetController = ( function() {
+
+} )();
+
+/*------------------------------------------------------------------------------
+USER INTERFACE CONTROLLER
+------------------------------------------------------------------------------*/
+var uiController = ( function() {
+
+} )();
+
+/*------------------------------------------------------------------------------
+GLOBAL APP CONTROLLER
+------------------------------------------------------------------------------*/
+var controller = ( function( budgetCtrl, uiCtrl ) {
+
+    document.querySelector('.add__btn').addEventListener( 'click', function() {
+
+        // Get field input data:
+        // Add item to budget controller:
+
+    });
+
+} )( budgetController, uiController );
