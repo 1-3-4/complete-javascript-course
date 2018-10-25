c18ed32d5a72c9adfd7c8e120b33947818b7d0d0
134 <38720563+1-3-4@users.noreply.github.com>
2018-10-25 09:55:47 +0200

    Completed lecture 78.
    Has a problem with the eventlisteners when the .add__btn is  active (after it has been clicked/chosen by tabulating). Upon hitting enter both eventlisteners fire.

--- 6-budgety/lecture-78-setting-up-the-first-event-listeners/app.js
+++ 6-budgety/lecture-78-setting-up-the-first-event-listeners/app.js
@@ -17,11 +17,31 @@ GLOBAL APP CONTROLLER
 ------------------------------------------------------------------------------*/
 var controller = ( function( budgetCtrl, uiCtrl ) {
 
-    document.querySelector('.add__btn').addEventListener( 'click', function() {
+    var ctrlAddItem = function () {
 
         // Get field input data:
         // Add item to budget controller:
+        // Add new item to UI:
+        // Calculate budget:
+        // Display budget in the UI:
 
-    });
+        console.log('!!!');
+
+    }
+
+    // Event listener for btn click:
+    document.querySelector('.add__btn').addEventListener( 'click', ctrlAddItem );
+
+    // Event listener for enter key press:
+    document.addEventListener( 'keypress', function( event ) {
+
+        // Check if pressed key is enter:
+        if ( event.keyCode === 13 || event.which === 13 ) {
+
+            ctrlAddItem();
+
+        }
+
+    } );
 
 } )( budgetController, uiController );
